bbbeb63b2616c8713f8b0054b7049ff533bd499c
Vladimir Osipov <vladimir.osipov@coursio.com>
2020-01-25 22:09:47 +0200

    shorter copies

--- en.js
+++ en.js
@@ -168,7 +168,7 @@ export default {
       'Need to share a copied image? Ctrl+V in the chat, we\'ll cover the rest.',
       'Click your name to change it both locally and for your peers.',
       'Ever needed to record your screen with just a few clicks? Try our screen recording plugin.',
-      'If you need to sketch something during a call try out the sketching plugin. Don\'t forget to turn on screen sharing.',
+      'Need to sketch during a call — try out the sketching plugin. Don\'t forget to turn on screen sharing.',
     ]
   },
 }
